a254dbe1077c7269be3177428ed266099d0e16f9
João Paulo Pulga <joaopaulo1339@outlook.com>
2019-03-06 23:37:28 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use sha3::Digest;
+use std::io;
+
+mod sha3_utils;
+
+fn main() {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            let mut hasher = sha3_utils::sha_512();
+
+            // write input message
+            hasher.input(&input.as_bytes()[0 .. input.len() - 1]);
+
+            for byte in hasher.result() {
+                print!("{:02x}", byte);
+            }
+
+            println!("");
+        }
+        Err(err) => println!("error: {}", err),
+    }
+}
---
+++ src/sha3_utils.rs
@@ -0,0 +1,17 @@
+use sha3::{Digest, Sha3_224, Sha3_256, Sha3_384, Sha3_512};
+
+pub fn sha3_224() -> Sha3_224 {
+    Sha3_224::new()
+}
+
+pub fn sha3_256() -> Sha3_256 {
+    Sha3_256::new()
+}
+
+pub fn sha3_384() -> Sha3_384 {
+    Sha3_384::new()
+}
+
+pub fn sha_512() -> Sha3_512 {
+    Sha3_512::new()
+}
