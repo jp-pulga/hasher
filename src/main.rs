e2c5b462df1bdc4067cafa459a400076732effe6
João Paulo Pulga <pulgovisk@protonmail.com>
2019-03-12 21:12:04 -0300

    Added --hide for hiding user input

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 use clap::{App, Arg};
 use digest::Digest;
-use sha3;
 use ripemd160;
 use ripemd320;
+use sha3;
 use std::io::*;
 
-fn hash_and_print<D: Digest>(to_hash: &str) {
+fn hash_and_print<D: Digest>(to_hash: String) {
     let mut hasher = D::new();
     hasher.input(to_hash.as_bytes());
 
     for byte in hasher.result() {
         print!("{:02x}", byte);
+    }
+}
+
+fn match_hash(hash : &str, to_hash: String) {
+    match hash {
+        // Sha3
+        "sha3_224" => hash_and_print::<sha3::Sha3_224>(to_hash),
+        "sha3_256" => hash_and_print::<sha3::Sha3_256>(to_hash),
+        "sha3_384" => hash_and_print::<sha3::Sha3_384>(to_hash),
+
+        //ripemd
+        "ripemd160" => hash_and_print::<ripemd160::Ripemd160>(to_hash),
+        "ripemd320" => hash_and_print::<ripemd320::Ripemd320>(to_hash),
+        _ => hash_and_print::<sha3::Sha3_512>(to_hash),
     }
 }
 
@@ -21,11 +35,11 @@ fn main() {
         .about("Hash tool made with rust")
         .arg(
             Arg::with_name("hash")
-                .short("-h")
+                .short("h")
                 .long("hash")
                 .value_name("Hash type")
                 .long_help(
-"The supported list of hashes is
+                    "The supported list of hashes is
 Sha3:
     sha3_224
     sha3_256
@@ -34,23 +48,23 @@ Sha3:
     
 ripemd:
     ripemd160
-    ripemd320")
+    ripemd320",
+                )
                 .takes_value(true),
         )
+        .arg(Arg::with_name("hide").long("hide"))
         .get_matches();
 
-    let stdin = stdin();
-    for line in stdin.lock().lines() {
-        match matches.value_of("hash").unwrap_or("sha3_512") {
-            // Sha3
-            "sha3_224" => hash_and_print::<sha3::Sha3_224>(&line.unwrap()),
-            "sha3_256" => hash_and_print::<sha3::Sha3_256>(&line.unwrap()),
-            "sha3_384" => hash_and_print::<sha3::Sha3_384>(&line.unwrap()),
+    let hash = matches.value_of("hash").unwrap_or("sha3_512");
 
-            //ripemd
-            "ripemd160" => hash_and_print::<ripemd160::Ripemd160>(&line.unwrap()),
-            "ripemd320" => hash_and_print::<ripemd320::Ripemd320>(&line.unwrap()),
-            _ => hash_and_print::<sha3::Sha3_512>(&line.unwrap()),
+    if matches.is_present("hide") {
+        use rpassword;
+
+        match_hash(hash, rpassword::read_password().unwrap());
+    } else {
+        let stdin = stdin();
+        for line in stdin.lock().lines() {
+            match_hash(hash, line.unwrap());
         }
     }
 
